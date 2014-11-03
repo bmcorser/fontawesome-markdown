3b56dd8527ba1ec2b76e7d08adcbf62bccba1833
Alex Charron <alex@undeterminant.net>
2014-11-02 22:11:48 -0500

    Python 3, avoid list that can get out-of-date.

--- fontawesome_markdown/__init__.py
+++ fontawesome_markdown/__init__.py
@@ -1,7 +1,7 @@
+from __future__ import unicode_literals
 from markdown.extensions import Extension
 from markdown.inlinepatterns import Pattern
 from markdown.util import etree
-from icon_list import icons
 
 fontawesome_pattern = r':(fa-[-\w]+):'
 
@@ -9,13 +9,10 @@ class FontAwesomePattern(Pattern):
     def handleMatch(self, m):
         el = etree.Element('i')
         icon_name = m.group(2)
-        if icon_name in icons:
-            el.attrib = {'class':'fa {0}'.format(icon_name)}
-            return el
-        return m.string
+        el.set('class', 'fa ' + icon_name)
+        return el
 
 class FontAwesomeExtension(Extension):
-
     def extendMarkdown(self, md, md_globals):
         fontawesome = FontAwesomePattern(fontawesome_pattern)
         md.inlinePatterns.add('fontawesome', fontawesome, '<reference')
