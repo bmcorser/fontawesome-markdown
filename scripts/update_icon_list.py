9caae45f76622e835e46684fb8112116336c189d
bmcorser <benmarshallcorser@gmail.com>
2014-11-28 09:00:32 +0000

    2to3 -- `map` returns a `map` object in 3k

--- scripts/update_icon_list.py
+++ scripts/update_icon_list.py
@@ -12,7 +12,7 @@ def main():
     icons_list = yaml.load(requests.get(URI).text)['icons']
     names_aliases = lambda I: itertools.chain([I['id']],
                                               I.get('aliases', list()))
-    names = tuple(itertools.chain(*map(names_aliases, icons_list)))
+    names = tuple(itertools.chain(*tuple(map(names_aliases, icons_list))))
     with open('../fontawesome_markdown/icon_list.py', 'w') as icons_list_py:
         icons_list_py.write('from __future__ import unicode_literals\n')
         icons_list_py.write('icons = \\\n')
