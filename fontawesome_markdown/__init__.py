90db7ae2652aded2bb2c26a162288b080c0610f6
bcaller <bcaller@users.noreply.github.com>
2016-02-10 01:53:10 +0000

    Allow importing via extension name

--- fontawesome_markdown/__init__.py
+++ fontawesome_markdown/__init__.py
@@ -1,1 +1,1 @@
-from .main import FontAwesomePattern, FontAwesomeExtension, FontAwesomeException  # NOQA
+from .main import FontAwesomePattern, FontAwesomeExtension, FontAwesomeException, makeExtension  # NOQA
