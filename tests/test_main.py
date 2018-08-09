6840464ac381622caaa865fd922c14227de03c91
Wataru Haniyama <haniyama_wataru@sgk.jp>
2018-08-09 11:06:19 +0900

    add unit tests

--- tests/test_main.py
+++ tests/test_main.py
@@ -20,3 +20,49 @@ def test_unknown_raises(fa_markdown):
     with pytest.raises(FontAwesomeException) as exc:
         fa_markdown.convert("i ♥ :fa-{0}:".format(unknown_icon))
         assert unknown_icon in exc.message
+
+
+def test_size(fa_markdown):
+    expected_markup = '<p>i ♥ <i class="fa fa-coffee fa-xs"></i></p>'
+    assert fa_markdown.convert('i ♥ :fa-coffee fa-xs:') == expected_markup
+
+    expected_markup = '<p>i ♥ <i class="fa fa-coffee fa-sm"></i></p>'
+    assert fa_markdown.convert('i ♥ :fa-coffee fa-sm:') == expected_markup
+
+    expected_markup = '<p>i ♥ <i class="fa fa-coffee fa-lg"></i></p>'
+    assert fa_markdown.convert('i ♥ :fa-coffee fa-lg:') == expected_markup
+
+    expected_markup = '<p>i ♥ <i class="fa fa-coffee fa-1x"></i></p>'
+    assert fa_markdown.convert('i ♥ :fa-coffee fa-1x:') == expected_markup
+
+    expected_markup = '<p>i ♥ <i class="fa fa-coffee fa-3x"></i></p>'
+    assert fa_markdown.convert('i ♥ :fa-coffee fa-3x:') == expected_markup
+
+    expected_markup = '<p>i ♥ <i class="fa fa-coffee fa-10x"></i></p>'
+    assert fa_markdown.convert('i ♥ :fa-coffee fa-10x:') == expected_markup
+
+
+def test_fab_icon(fa_markdown):
+    expected_markup = '<p>i ♥ <i class="fab fa-facebook"></i></p>'
+    assert fa_markdown.convert('i ♥ :fa-facebook:') == expected_markup
+
+
+def test_fab_icon_with_prefix(fa_markdown):
+    expected_markup = '<p>i ♥ <i class="fab fa-facebook"></i></p>'
+    assert fa_markdown.convert('i ♥ :fab fa-facebook:') == expected_markup
+
+
+def test_far_icon_with_prefix(fa_markdown):
+    expected_markup = '<p>i ♥ <i class="far fa-font-awesome-logo-full"></i></p>'
+    assert fa_markdown.convert('i ♥ :far fa-font-awesome-logo-full:') == expected_markup
+
+
+def test_fa_icon_without_prefix(fa_markdown):
+    expected_markup = '<p>i ♥ <i class="fa fa-font-awesome-logo-full"></i></p>'
+    assert fa_markdown.convert('i ♥ :fa-font-awesome-logo-full:') == expected_markup
+
+
+# light style not found in icons.json...
+# def test_fal_icon_with_prefix(fa_markdown):
+    # expected_markup = '<p>i ♥ <i class="fal fa-font-awesome-logo-full"></i></p>'
+    # assert fa_markdown.convert('i ♥ :fal fa-address-book:') == expected_markup
